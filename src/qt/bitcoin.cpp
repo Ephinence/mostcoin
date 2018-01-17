6e6d6bb01a7af532fd085a8fc2d2d5fafcd86560
mostcoin <34260529+mostcoin@users.noreply.github.com>
2018-01-17 11:28:36 +0900

    Update bitcoin.cpp

--- src/qt/bitcoin.cpp
+++ src/qt/bitcoin.cpp
@@ -155,7 +155,7 @@ int main(int argc, char *argv[])
     // Application identification (must be set before OptionsModel is initialized,
     // as it is used to locate QSettings)
     QApplication::setOrganizationName("MostCoin");
-    QApplication::setOrganizationDomain("MostCoin.org");
+    QApplication::setOrganizationDomain("MostCoin.net");
     if(GetBoolArg("-testnet")) // Separate UI settings for testnet
         QApplication::setApplicationName("MostCoin-Qt-testnet");
     else
